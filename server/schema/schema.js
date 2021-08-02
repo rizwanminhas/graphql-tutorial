34664c580a3072394a182e6ea32e984a5154cb81
rizwan <rizwan@outlook.com>
2021-08-02 13:24:33 -0400

    add Author schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,17 @@
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql
 
 const books = [
-    {id: "1", name:"book 1", genre: "horror"},
-    {id: "2", name:"book 2", genre: "action"},
-    {id: "3", name:"book 3", genre: "history"},
+    { id: "1", name: "book 1", genre: "horror" },
+    { id: "2", name: "book 2", genre: "action" },
+    { id: "3", name: "book 3", genre: "history" },
+]
+
+const authors = [
+    { id: "1", name: "author 1", age: 30 },
+    { id: "2", name: "author 2", age: 40 },
+    { id: "3", name: "author 3", age: 50 },
 ]
 
 const BookType = new GraphQLObjectType({
@@ -17,6 +23,16 @@ const BookType = new GraphQLObjectType({
     })
 })
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+})
+
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -24,8 +40,16 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                const result =  books.filter(book => book.id === args.id)
-                return result.length > 0 ? result[0] : null       
+                const result = books.filter(book => book.id === args.id)
+                return result.length > 0 ? result[0] : null
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                const result = authors.filter(author => author.id === args.id)
+                return result.length > 0 ? result[0] : null
             }
         }
     }
